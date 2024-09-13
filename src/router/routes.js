5fbfab54ba2bc405b9f9f2920a8cba43ab5cee2a
iWutang <158086875+iWutang@users.noreply.github.com>
2024-09-13 15:42:42 +0200

    1.3.1

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,12 +3,15 @@ import { createWebHistory } from "vue-router"
 
 import mainPage from "@/views/mainPage.vue"
 import projet1Page from "@/views/projet1Page.vue"
+import projet2Page from "@/views/projet2Page.vue"
+import projet3Page from "@/views/projet3Page.vue"
+
 
 const routes = [
     { path: '/', name: "Accueil", component: mainPage},
     { path: '/projet1', name: "Projet-1", component: projet1Page},
-    { path: '/projet2', name: "Projet-2", component: projet1Page},
-    { path: '/projet3', name: "Projet-3", component: projet1Page},
+    { path: '/projet2', name: "Projet-2", component: projet2Page},
+    { path: '/projet3', name: "Projet-3", component: projet3Page},
 
 
 ]
---
+++ src/views/projet3Page.vue
@@ -0,0 +1,116 @@
+<template>
+    <section class="projet3" id="projet3">
+        <h2 class="heading">Projet 3</h2>
+
+        <div class="projet-container">
+            <div class="sneakpeak-box">
+                <div class="projet3-info">
+                    <h4>Partie HTML</h4>
+                    <p>image</p>
+                </div>
+            </div> 
+
+            <div class="sneakpeak-box">
+                <div class="projet3-info">
+                    <h4>Partie CSS</h4>
+                    <p>image</p>
+                </div>
+            </div>
+
+            <div class="sneakpeak-box">
+                <div class="projet3-info">
+                    <h4>Partie JavaScript</h4>
+                    <p>image</p>
+                </div>
+            </div>
+
+            <div class="sneakpeak-box">
+                <div class="projet3-info">
+                    <h4>Rendu Final</h4>
+                    <p>image</p>
+                </div>
+            </div>
+        </div>
+        <article>
+            <div class="sneakpeak-box">
+                <div class="projet3-info">
+                    <h4>Description du projet</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint incidunt unde harum distinctio quasi,
+                    facilis soluta officia ipsam ratione, rem blanditiis porro doloremque eaque, nihil molestiae nesciunt 
+                    reiciendis dolores dignissimos.</p>
+                </div>
+            </div>
+        </article>
+    </section>
+</template>
+
+<style>
+.projet3{
+    background: var(--bg-color);
+    color: black;
+}
+.projet3 h2{
+    margin-bottom: 5rem;
+    color: white;
+}
+.projet-container{
+    display: grid;
+    grid-template-columns: repeat(2,1fr);
+    align-items: center;
+    gap: 2.5rem;
+}
+.sneakpeak-box{
+    background-color: var(--main-color);
+    height: 300px;
+    border-radius: 3rem;
+    border: 5px solid transparent;
+    cursor: pointer;
+    transition: 0.4s ease-in-out;
+}
+.sneakpeak-box:hover{
+    background: white;
+    color: black;
+    border: 5px solid var(--main-color);
+    transform: scale(1.03);
+}
+.sneakpeak-box .service-info{
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    max-height: 200px;
+    justify-content: left;
+    align-items: baseline;
+    padding: 5rem 5rem;
+}
+.projet3-info h4{
+    font-size: 3rem;
+    font-weight: 800;
+    line-height: 2;
+}
+.projet3-info p{
+    font-size: 1.6rem;
+    font-weight: 600;
+    max-height: 100px;
+    line-height: 1.7;
+    margin: auto;
+}
+article{
+    margin-top: 5rem;
+}
+.sneakpeak-boX{
+    background-color: var(--main-color);
+    height: 300px;
+    margin-top: 5rem;
+    border-radius: 3rem;
+    border: 5px solid transparent;
+    cursor: pointer;
+    transition: 0.4s ease-in-out;
+}
+.sneakpeak-boX:hover{
+    background: white;
+    color: black;
+    border: 5px solid var(--main-color);
+    transform: scale(1.03);
+}
+
+</style>
